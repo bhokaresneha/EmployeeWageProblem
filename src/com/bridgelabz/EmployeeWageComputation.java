ca4fb3f80004f99cb1afdb9627d447aeefc58a24
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-26 13:53:45 +0530

    Added :Using Switch case Employee Wage Calculation

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,8 +1,32 @@
 package com.bridgelabz;
+import java.util.Random;
+public class EmployeeWageComputation {
+    // Variable initialization
+    static int attendance;
+    static  int WAGE_PER_HOUR = 20;
 
-    public class EmployeeWageComputation {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Problem");
+    public static void main(String[] args) {
+        // creating object of class
+        int workingHrs = 0;
+        // creating oject of random class
+        Random random = new Random();
+        //generate random no using random() function
+        attendance  = random.nextInt(3);
+        switch (attendance){
+            case 0 :
+                workingHrs = 0;
+                System.out.println("Employee is absent");
+                break;
+            case 1 :
+                workingHrs = 4;
+                System.out.println("Employee working as part-time");
+                break;
+            case 2 :
+                workingHrs = 8;
+                System.out.println("Employee working as full time");
+                break;
         }
+        int dailyWage = workingHrs * WAGE_PER_HOUR;
+        System.out.println("Daily wage is " + dailyWage);
     }
-
+}
