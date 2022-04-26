fd01a4d45bf743337a5853f553ffc80995439ab1
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-26 13:33:41 +0530

    Added : Employee Attendance

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
-    public class EmployeeWageComputation {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Problem");
-        }
-    }
+import java.util.Random;
 
+public class EmployeeWageComputation {
+    // Variable initialization
+    static int attendance;
+
+    public static void main(String[] args) {
+        // creating object of class
+        EmployeeWageComputation ew=new EmployeeWageComputation();
+
+        // creating oject of random class
+        Random random = new Random();
+        //generate random no using random() function
+        attendance  = random.nextInt(2);
+        //Check if employee is present
+        if (attendance == 1)
+            System.out.println("Employee is present.");
+        else
+            System.out.println("Employee is absent.");
+
+    }
+}
