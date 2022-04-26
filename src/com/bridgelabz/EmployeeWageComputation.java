61f02ba3adf91ecbf45c76e85394a90c5d85456d
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-26 13:46:13 +0530

    Added : Calculating Employee Daily Wage Part time

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,8 +1,33 @@
 package com.bridgelabz;
 
-    public class EmployeeWageComputation {
-        public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Computation Problem");
-        }
-    }
+import java.util.Random;
 
+public class EmployeeWageComputation {
+    // Variable initialization
+    static int attendance;
+    static  int WAGE_PER_HOUR = 20;
+
+    public static void main(String[] args) {
+        // creating object of class
+        EmployeeWageComputation ew=new EmployeeWageComputation();
+        int workingHrs = 0;
+        // creating oject of random class
+        Random random = new Random();
+        //generate random no using random() function
+        attendance = (int) Math.floor(Math.random() * 10) % 3;
+        if (attendance == 0){
+            System.out.println("Employee is absent");
+            workingHrs = 0;
+        }
+        else if (attendance == 1){
+            System.out.println("Employee working as part-time");
+            workingHrs = 4;
+        }
+        else{
+            System.out.println("Employee working as full time");
+            workingHrs = 8;
+        }
+        int dailyWage = workingHrs * WAGE_PER_HOUR;
+        System.out.println("Daily wage is " + dailyWage);
+    }
+}
